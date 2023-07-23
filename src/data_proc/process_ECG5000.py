1186914e2d496c2d24e71551c580efb393b47801
mz <35124505+michael-wzhu@users.noreply.github.com>
2023-07-23 21:08:25 +0800

    Update process_ECG5000.py

--- src/data_proc/process_ECG5000.py
+++ src/data_proc/process_ECG5000.py
@@ -5,68 +5,97 @@ import os
 task_type_prompt = "Task type: time series classfication"
 
 template_1 = """<query>
-下面的时间序列记录了心电图数据：<ts-data-blank-sep>。数据点用空格分割。
+下面的时间序列记录了一次心跳中的心电图数据：<ts-data-blank-sep>。数据来源于一位患有严重充血性心力衰竭的患者。
 您能判断这个时间序列描述的心跳活动是属于哪一类别的吗？
 <options>
-可能的类别有：正常心跳，R-on-T型室性早搏，室上型早搏，室性早搏，未分类心跳。
+可能的类别有：正常心跳，R-on-T型室性早搏，室上性早搏或异位搏动，室性早搏，其他类型心跳。
 请从上述选项中选择一个回答。
 <response>
 """
 
 list_templates_by_chatgpt = [
     """<query>
-The following time series records the ECG data: <ts-data-blank-sep>. Data points are separated by spaces.
+以下时间序列记录了一名患有严重充血性心力衰竭的患者的心电图数据：<ts-data-blank-sep>。
+现在，请您判断这个时间序列描述的心跳活动属于以下哪一类别？
+<options>
+可能的类别包括：正常心跳，R-on-T型室性早搏，室上性早搏或异位搏动，室性早搏，其他类型心跳。
+请从以上选项中选择一个答案。
+<response>
+    """,
+    """<query>
+下面的时间序列记录了一位严重充血性心力衰竭患者的心电图数据：<ts-data-blank-sep>。
+您能判断这个时间序列所描述的心跳活动属于哪一类别吗？
+<options>
+可选类别：正常心跳，R-on-T型室性早搏，室上性早搏或异位搏动，室性早搏，其他类型心跳。
+请从以上选项中选择一个答案。
+<response>
+    """,
+    """<query>
+这里记录了一组时间序列，反映了一名患有严重充血性心力衰竭的患者的心电图数据：<ts-data-blank-sep>。
+您能判定这组时间序列所描述的心跳活动属于哪种类别吗？
+<options>
+可供选择的类别包括：正常心跳，R-on-T型室性早搏，室上性早搏或异位搏动，室性早搏，其他类型心跳。
+请从以上选项中作出选择。
+<response>
+    """,
+
+    """<query>
+The following time series represents an electrocardiogram (ECG) data recorded during a heartbeat from a patient suffering from severe congestive heart failure: <ts-data-blank-sep>.
 Can you determine which category of heartbeat activity this time series describes?
 <options>
-Possible categories: Normal, R-on-T premature ventricular contraction, Supraventricular premature or ectopic beat, Premature ventricular contraction, Unclassified beat.
-Please select one answer from the options above.
+Possible categories include: Normal heartbeat, R-on-T premature ventricular contraction, Supraventricular premature or ectopic beat, Premature ventricular contraction, Other types of heartbeat.
+Please select one response from the options provided.
 <response>
-""",
+    """,
     """<query>
-我们有一个时间序列数据集（用空格分隔）：<ts-data-blank-sep>。它记录了心电图数据。请您判断这些数据属于哪一类心跳活动？
+The given time series records the electrocardiogram (ECG) data of a patient experiencing severe congestive heart failure during a heartbeat: <ts-data-blank-sep>.
+Could you identify the specific category to which this heartbeat activity belongs?
 <options>
-可能的类别有：正常心跳，R-on-T型室性早搏，室上型早搏，室性早搏，未分类心跳。
-请从上述选项中选择一个回答。
+Potential categories are: Normal heartbeat, R-on-T premature ventricular contraction, Supraventricular premature or ectopic beat, Premature ventricular contraction, Other types of heartbeat.
+Please choose a response from the provided options.
 <response>
-""",
+    """,
     """<query>
-We have a time series dataset (separated by spaces): <ts-data-blank-sep>. It records ECG data. Could you please determine the category of heartbeat activity for this data?
+In the provided time series, you can find the electrocardiogram (ECG) data from a patient with severe congestive heart failure during a heartbeat: <ts-data-blank-sep>.
+Can you categorize the type of heartbeat activity represented in this time series?
 <options>
-Possible categories: Normal, R-on-T premature ventricular contraction, Supraventricular premature or ectopic beat, Premature ventricular contraction, Unclassified beat.
-Please select one answer from the options above.
+You may choose from the following categories: Normal heartbeat, R-on-T premature ventricular contraction, Supraventricular premature or ectopic beat, Premature ventricular contraction, Other types of heartbeat.
+Please select one option as your response.
 <response>
-""",
+    """,
     """<query>
-以下时间序列记录了心电图数据：<ts-data-blank-sep>，其中数据点以空格分隔。请问您能确定这个时间序列描述的心跳活动属于哪一个类别？
+This time series presents the electrocardiogram (ECG) data recorded during a heartbeat from a patient afflicted with severe congestive heart failure: <ts-data-blank-sep>.
+Would you be able to identify the category to which this particular heartbeat activity belongs?
 <options>
-可能的类别有：正常心跳，R-on-T型室性早搏，室上型早搏，室性早搏，未分类心跳。
-请从上述选项中选择一个回答。
+Possible categories include: Normal heartbeat, R-on-T premature ventricular contraction, Supraventricular premature or ectopic beat, Premature ventricular contraction, Other types of heartbeat.
+Kindly choose one response from the options provided.
 <response>
-""",
+    """,
     """<query>
-The following time series records the ECG data: <ts-data-blank-sep>, with data points separated by spaces. Can you determine which category of heartbeat activity this time series describes?
+The provided time series displays the electrocardiogram (ECG) data of a patient suffering from severe congestive heart failure during a heartbeat: <ts-data-blank-sep>.
+Your task is to determine the specific category of heartbeat activity described in this time series.
 <options>
-Possible categories: Normal, R-on-T premature ventricular contraction, Supraventricular premature or ectopic beat, Premature ventricular contraction, Unclassified beat.
-Please select one answer from the options above.
+Available categories are: Normal heartbeat, R-on-T premature ventricular contraction, Supraventricular premature or ectopic beat, Premature ventricular contraction, Other types of heartbeat.
+Please make your selection from the given options.
 <response>
-""",
+    """,
 
 
 ]
 
 label_map = {
-    "1": "Normal",
+    "1": "Normal heartbeat",
     "2": "R-on-T premature ventricular contraction",
     "3": "Supraventricular premature or ectopic beat",
     "4": "Premature ventricular contraction",
-    "5": "Unclassified beat",
+    "5": "Other types of heartbeat",
 }
 label_map_zh = {
     "1": "正常心跳",
     "2": "R-on-T型室性早搏",
-    "3": "室上型早搏",
+    "3": "室上性早搏或异位搏动",
     "4": "室性早搏",
-    "5": "未分类心跳",
+    "5": "其他类型心跳",  
 }
 
 
